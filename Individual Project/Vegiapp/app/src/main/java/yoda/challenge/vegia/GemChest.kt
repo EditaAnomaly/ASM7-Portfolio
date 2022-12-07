0defde3dcf3225481a233bbff2721b22a6549f08
Edita <102149932+EditaAnomaly@users.noreply.github.com>
2022-12-07 11:03:10 +0100

    tip icon and gem chest layout

---
+++ Individual Project/Vegiapp/app/src/main/java/yoda/challenge/vegia/GemChest.kt
@@ -0,0 +1,11 @@
+package yoda.challenge.vegia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GemChest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.`gem_chest.xml`)
+    }
+}
