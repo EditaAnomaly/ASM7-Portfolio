20eaa4115978c22fcdef766bdaa46aa5a5b2b63f
Edita <102149932+EditaAnomaly@users.noreply.github.com>
2022-12-15 16:56:39 +0100

    fixed image fetching from db

--- Individual Project/Vegiapp/app/src/main/java/yoda/challenge/vegia/GemChest.kt
+++ Individual Project/Vegiapp/app/src/main/java/yoda/challenge/vegia/GemChest.kt
@@ -35,80 +35,131 @@ class GemChest : Fragment() {
         when (x) {
             1 -> {
                 gem1_iv.setImageResource(R.drawable.chal1)
-                gemClick(gem1_iv, x)
+                gemClick(gem1_iv, 1)
             }
             2 -> {
                 gem1_iv.setImageResource(R.drawable.chal1)
-                gemClick(gem1_iv, x)
+                gemClick(gem1_iv, 1)
                 gem2_iv.setImageResource(R.drawable.chal2)
+                gemClick(gem2_iv, 2)
             }
             3 -> {
                 gem1_iv.setImageResource(R.drawable.chal1)
-                gemClick(gem1_iv, x)
-                gem2_iv.setImageResource(R.drawable.chal3)
-                gem3_iv.setImageResource(R.drawable.chal4)
+                gemClick(gem1_iv, 1)
+                gem2_iv.setImageResource(R.drawable.chal2)
+                gemClick(gem2_iv, 2)
+                gem3_iv.setImageResource(R.drawable.chal3)
+                gemClick(gem3_iv, 3)
             }
             4 -> {
                 gem1_iv.setImageResource(R.drawable.chal1)
+                gemClick(gem1_iv, 1)
                 gem2_iv.setImageResource(R.drawable.chal2)
+                gemClick(gem2_iv, 2)
                 gem3_iv.setImageResource(R.drawable.chal3)
+                gemClick(gem3_iv, 3)
                 gem4_iv.setImageResource(R.drawable.chal4)
+                gemClick(gem4_iv, 4)
             }
             5 -> {
                 gem1_iv.setImageResource(R.drawable.chal1)
+                gemClick(gem1_iv, 1)
                 gem2_iv.setImageResource(R.drawable.chal2)
+                gemClick(gem2_iv, 2)
                 gem3_iv.setImageResource(R.drawable.chal3)
+                gemClick(gem3_iv, 3)
                 gem4_iv.setImageResource(R.drawable.chal4)
+                gemClick(gem4_iv, 4)
                 gem5_iv.setImageResource(R.drawable.chal5)
+                gemClick(gem5_iv, 5)
             }
             6 -> {
                 gem1_iv.setImageResource(R.drawable.chal1)
+                gemClick(gem1_iv, 1)
                 gem2_iv.setImageResource(R.drawable.chal2)
+                gemClick(gem2_iv, 2)
                 gem3_iv.setImageResource(R.drawable.chal3)
+                gemClick(gem3_iv, 3)
                 gem4_iv.setImageResource(R.drawable.chal4)
+                gemClick(gem4_iv, 4)
                 gem5_iv.setImageResource(R.drawable.chal5)
+                gemClick(gem5_iv, 5)
                 gem6_iv.setImageResource(R.drawable.chal6)
+                gemClick(gem6_iv, 6)
             }
             7 -> {
                 gem1_iv.setImageResource(R.drawable.chal1)
+                gemClick(gem1_iv, 1)
                 gem2_iv.setImageResource(R.drawable.chal2)
+                gemClick(gem2_iv, 2)
                 gem3_iv.setImageResource(R.drawable.chal3)
+                gemClick(gem3_iv, 3)
                 gem4_iv.setImageResource(R.drawable.chal4)
+                gemClick(gem4_iv, 4)
                 gem5_iv.setImageResource(R.drawable.chal5)
+                gemClick(gem5_iv, 5)
                 gem6_iv.setImageResource(R.drawable.chal6)
+                gemClick(gem6_iv, 6)
                 gem7_iv.setImageResource(R.drawable.chal7)
+                gemClick(gem7_iv, 7)
             }
             8 -> {
                 gem1_iv.setImageResource(R.drawable.chal1)
+                gemClick(gem1_iv, 1)
                 gem2_iv.setImageResource(R.drawable.chal2)
+                gemClick(gem2_iv, 2)
                 gem3_iv.setImageResource(R.drawable.chal3)
+                gemClick(gem3_iv, 3)
                 gem4_iv.setImageResource(R.drawable.chal4)
+                gemClick(gem4_iv, 4)
                 gem5_iv.setImageResource(R.drawable.chal5)
+                gemClick(gem5_iv, 5)
                 gem6_iv.setImageResource(R.drawable.chal6)
+                gemClick(gem6_iv, 6)
                 gem7_iv.setImageResource(R.drawable.chal7)
+                gemClick(gem7_iv, 7)
                 gem8_iv.setImageResource(R.drawable.chal8)
+                gemClick(gem8_iv, 8)
             }
             9 -> {
                 gem1_iv.setImageResource(R.drawable.chal1)
+                gemClick(gem1_iv, 1)
                 gem2_iv.setImageResource(R.drawable.chal2)
+                gemClick(gem2_iv, 2)
                 gem3_iv.setImageResource(R.drawable.chal3)
+                gemClick(gem3_iv, 3)
                 gem4_iv.setImageResource(R.drawable.chal4)
+                gemClick(gem4_iv, 4)
                 gem5_iv.setImageResource(R.drawable.chal5)
+                gemClick(gem5_iv, 5)
                 gem6_iv.setImageResource(R.drawable.chal6)
+                gemClick(gem6_iv, 6)
                 gem7_iv.setImageResource(R.drawable.chal7)
+                gemClick(gem7_iv, 7)
                 gem8_iv.setImageResource(R.drawable.chal8)
+                gemClick(gem8_iv, 8)
                 gem9_iv.setImageResource(R.drawable.chal9)
+                gemClick(gem9_iv, 9)
             }
             10 -> {
                 gem1_iv.setImageResource(R.drawable.chal1)
+                gemClick(gem1_iv, 1)
                 gem2_iv.setImageResource(R.drawable.chal2)
+                gemClick(gem2_iv, 2)
                 gem3_iv.setImageResource(R.drawable.chal3)
+                gemClick(gem3_iv, 3)
                 gem4_iv.setImageResource(R.drawable.chal4)
+                gemClick(gem4_iv, 4)
                 gem5_iv.setImageResource(R.drawable.chal5)
+                gemClick(gem5_iv, 5)
                 gem6_iv.setImageResource(R.drawable.chal6)
+                gemClick(gem6_iv, 6)
                 gem7_iv.setImageResource(R.drawable.chal7)
+                gemClick(gem7_iv, 7)
                 gem8_iv.setImageResource(R.drawable.chal8)
+                gemClick(gem8_iv, 8)
                 gem9_iv.setImageResource(R.drawable.chal9)
+                gemClick(gem9_iv, 9)
                 gem10_iv.setImageResource(R.drawable.chal10)
             }
         }
@@ -116,7 +167,6 @@ class GemChest : Fragment() {
             gem_memory.visibility = View.INVISIBLE
         }
     }
-
 
     fun gemClick(imageView: ImageView, x: Int) {
         imageView.setOnClickListener {
